55143a9436533afff91a806a50ee3a798870f9bb
Jade Hernandez <jade.hernandez88@gmail.com>
2026-05-26 17:13:30 +0200

    feat(app): add Navbar component to the main application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Navbar } from "./blocks/navigation/Navbar";
+
 function App() {
   return (
     <div
       id='page-body'
       className='mx-auto flex min-h-dvh w-full min-w-dvw flex-col items-center bg-linear-to-r from-[#F9FAFB] to-[#D2D6DB] p-4'
     >
+      <Navbar />
       <div className='mt-4 flex w-full flex-col rounded-md bg-white'>
         <main className='flex flex-col'></main>
       </div>
