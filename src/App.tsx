e41f86137de17cefef27937bcbace64d9c17f6b8
Jade Hernandez <jade.hernandez88@gmail.com>
2026-05-26 16:59:19 +0200

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div
+      id='page-body'
+      className='mx-auto flex min-h-dvh w-full min-w-dvw flex-col items-center bg-linear-to-r from-[#F9FAFB] to-[#D2D6DB] p-4'
+    >
+      <div className='mt-4 flex w-full flex-col rounded-md bg-white'>
+        <main className='flex flex-col'></main>
+      </div>
+    </div>
+  );
+}
+
+export { App };
